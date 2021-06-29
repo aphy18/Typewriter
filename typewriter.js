85e66c90770bc35fa37af2ed522461db47508d67
aphy18 <aphason1@.com>
2021-06-29 17:30:57 +0000

    animation!

---
+++ typewriter.js
@@ -0,0 +1,20 @@
+const sentence = "hello there from lighthouse labs ";
+
+for(let i =0; i < sentence.length; i++) {
+    setTimeout(() => {
+        process.stdout.write(sentence[i])
+    }, i * 50);
+    
+}
+
+    setTimeout(() => {
+       process.stdout.write("\n") // logs sentence      
+    },1550)
+
+
+
+
+
+
+
+
